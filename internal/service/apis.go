7b031e74a7f511bf358df5e67d4a2599fb70ecf3
vadimpk <vadyman.pk@gmail.com>
2023-07-02 17:50:21 +0300

    feat: add chain of responsibility for crypto providers

--- internal/service/apis.go
+++ internal/service/apis.go
@@ -1,10 +1,13 @@
 package service
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 type APIs struct {
-	Email  EmailAPI
-	Crypto CryptoAPI
+	Email           EmailAPI
+	CryptoProviders CryptoAPIProviders
 }
 
 // EmailAPI provides methods for sending emails that are used in EmailService and
@@ -25,4 +28,63 @@ type SendOptions struct {
 // implemented in external packages.
 type CryptoAPI interface {
 	GetRate(ctx context.Context, fromCurrency, toCurrency string) (float64, error)
+}
+
+// CryptoAPIProviderType represents type of 3rd party provider of crypto API.
+type CryptoAPIProviderType string
+
+const (
+	CryptoAPIProviderCoinbase  CryptoAPIProviderType = "coinbase"
+	CryptoAPIProviderCoinAPI   CryptoAPIProviderType = "coinapi"
+	CryptoAPIProviderCoinGecko CryptoAPIProviderType = "coingecko"
+)
+
+// CryptoAPIProviders is used to store all available crypto APIs.
+type CryptoAPIProviders map[CryptoAPIProviderType]CryptoAPI
+
+// CryptoAPIChain is used to create chain of responsibility for crypto APIs.
+type CryptoAPIChain struct {
+	api  CryptoAPI
+	next *CryptoAPIChain
+}
+
+// NewCryptoAPIChain creates chain of responsibility for crypto APIs.
+// It uses order to determine which API to use first. If no order is provided, default order is used.
+func NewCryptoAPIChain(providers CryptoAPIProviders, order ...CryptoAPIProviderType) (*CryptoAPIChain, error) {
+	var lastProvider *CryptoAPIChain
+
+	// default order
+	if len(order) == 0 {
+		order = []CryptoAPIProviderType{
+			CryptoAPIProviderCoinAPI,
+			CryptoAPIProviderCoinGecko,
+			CryptoAPIProviderCoinbase,
+		}
+	}
+
+	for i := len(order) - 1; i >= 0; i-- {
+		provider := order[i]
+		api, ok := providers[provider]
+		if !ok {
+			return nil, fmt.Errorf("unknown provider: %s", provider)
+		}
+		lastProvider = &CryptoAPIChain{
+			api:  api,
+			next: lastProvider,
+		}
+	}
+
+	if lastProvider == nil {
+		return nil, fmt.Errorf("no providers")
+	}
+
+	return lastProvider, nil
+}
+
+func (chain *CryptoAPIChain) GetRate(ctx context.Context, fromCurrency, toCurrency string) (float64, error) {
+	rate, err := chain.api.GetRate(ctx, fromCurrency, toCurrency)
+	if err != nil && chain.next != nil {
+		return chain.next.GetRate(ctx, fromCurrency, toCurrency)
+	}
+	return rate, err
 }
